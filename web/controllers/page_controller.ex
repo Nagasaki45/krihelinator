25f8e4fa21504a0017e5d6a40fa472dc48766ae3
Tom Gurion <nagasaki45@gmail.com>
2016-07-10 20:08:53 +0300

    Through together a basic frontend

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule Krihelinator.PageController do
   use Krihelinator.Web, :controller
+  alias Krihelinator.Repo
+  alias Krihelinator.GithubRepo
+  alias Krihelinator.Krihelimeter
 
   def index(conn, _params) do
-    render conn, "index.html"
+    repos =
+      Repo.all(GithubRepo)
+      |> Enum.sort_by(&Krihelimeter.calculate/1, &>=/2)  # Descending
+    render conn, "index.html", repos: repos
   end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule Krihelinator.PageView do
   use Krihelinator.Web, :view
+
+  def krihelimeter(repo) do
+    Krihelinator.Krihelimeter.calculate(repo)
+  end
 end
