67f59ff88c451d7a57c639c6dcf1331b054e900a
Tom Gurion <nagasaki45@gmail.com>
2016-07-21 12:37:52 +0300

    Limit presentation to 50 repos. Fix #16

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,8 @@ defmodule Krihelinator.PageController do
   use Krihelinator.Web, :controller
 
   def index(conn, _params) do
-    repos = Repo.all(from GithubRepo, order_by: [desc: :krihelimeter])
+    query = from GithubRepo, order_by: [desc: :krihelimeter], limit: 50
+    repos = Repo.all(query)
     render conn, "index.html", repos: repos
   end
 end
