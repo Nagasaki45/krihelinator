b6377d11b2f15533726e28fd030fe32d3184589b
Tom Gurion <nagasaki45@gmail.com>
2016-07-22 14:06:07 +0300

    No need to set the krihelimeter in PostScrapeProcess

--- lib/pipeline/post_scraper_process.ex
+++ lib/pipeline/post_scraper_process.ex
@@ -2,9 +2,10 @@ alias Experimental.GenStage
 
 defmodule Krihelinator.Pipeline.PostScraperProcess do
   use GenStage
+  alias Krihelinator.Krihelimeter
 
   @moduledoc """
-  Set the krihelimeter, filter projects below statistics thresholds.
+  Filter out projects below statistics thresholds.
   """
 
   def init([]) do
@@ -12,13 +13,9 @@ defmodule Krihelinator.Pipeline.PostScraperProcess do
   end
 
   def handle_events(repos, _from, threshold) do
-    repos = repos
-      |> Stream.map(&set_krihelimeter/1)
-      |> Enum.filter(fn repo -> repo.krihelimeter > threshold end)
+    repos =
+      repos
+      |> Enum.filter(fn r -> Krihelimeter.calculate(r) > threshold end)
     {:noreply, repos, threshold}
-  end
-
-  def set_krihelimeter(repo) do
-    Map.put(repo, :krihelimeter, Krihelinator.Krihelimeter.calculate(repo))
   end
 end
