e775980b5ce1d0f127c99918f52c2213b273890f
Tom Gurion <nagasaki45@gmail.com>
2016-07-28 12:44:29 +0300

    Scripts: Refactor delete_zero_commits_repos to be more readable

--- priv/scripts/delete_zero_commits_repos.exs
+++ priv/scripts/delete_zero_commits_repos.exs
@@ -1,9 +1,8 @@
 alias Krihelinator.{Repo, GithubRepo}
 
 for repo <- Repo.all(GithubRepo) do
-  commits = repo.commits
-  unless commits > 0 do
-    IO.puts "#{repo.name} have only #{commits} commits, deleting!"
+  if repo.commits == 0 do
+    IO.puts "#{repo.name} have zero commits, deleting!"
     Repo.delete!(repo)
   end
 end
