df207b9c37dcc127d16aa76fa39bb7430cf029f0
Tom Gurion <nagasaki45@gmail.com>
2016-08-03 15:45:14 +0300

    Enable gzip on statics. Fix #44

--- lib/krihelinator/endpoint.ex
+++ lib/krihelinator/endpoint.ex
@@ -8,7 +8,7 @@ defmodule Krihelinator.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :krihelinator, gzip: false,
+    at: "/", from: :krihelinator, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
