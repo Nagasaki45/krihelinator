ec2db169f025553af0c17ca32167c504b0c7243a
Tom Gurion <nagasaki45@gmail.com>
2016-12-19 21:42:42 +0000

    Upgrade phoenix_ecto to get ecto 2.1 with time fields

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Krihelinator.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.0"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
+     {:phoenix_ecto, "~> 3.1"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
