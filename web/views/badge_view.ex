9b823277b79fb5fac0522206eb0c6021e93d2536
Tom Gurion <nagasaki45@gmail.com>
2016-12-19 12:51:10 +0000

    Replace Krihelimeter red button with badge. Fix #56

--- web/views/badge_view.ex
+++ web/views/badge_view.ex
@@ -1,8 +1,12 @@
 defmodule Krihelinator.BadgeView do
   use Krihelinator.Web, :view
 
-  def render("badge.svg", %{repo: repo}) do
-    krihelimeter = Krihelinator.Krihelimeter.calculate(repo)
+  @doc """
+  Returns the Vue.js template for presenting the badge. It is used most
+  of the time. The render function replaces the Vue tags with the correct
+  value manually.
+  """
+  def template() do
     """
     <svg xmlns="http://www.w3.org/2000/svg" width="120" height="20">
       <linearGradient id="a" x2="0" y2="100%">
@@ -18,11 +22,19 @@ defmodule Krihelinator.BadgeView do
         <text x="4" y="14">Krihelimeter</text>
       </g>
       <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-        <text x="100" y="15" fill="#010101" fill-opacity=".3">#{krihelimeter}</text>
-        <text x="100" y="14">#{krihelimeter}</text>
+        <text x="100" y="15" fill="#010101" fill-opacity=".3">{{ repo.krihelimeter }}</text>
+        <text x="100" y="14">{{ repo.krihelimeter }}</text>
       </g>
     </svg>
     """
+  end
+
+  def render("badge.svg", %{repo: repo}) do
+    krihelimeter_str =
+      repo
+      |> Krihelinator.Krihelimeter.calculate()
+      |> Integer.to_string()
+    String.replace(template, "{{ repo.krihelimeter }}", krihelimeter_str)
   end
 
   def render("error.json", _opts) do
