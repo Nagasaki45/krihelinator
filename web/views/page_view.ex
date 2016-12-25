f43fff0f808067ab2644633e0cdb36254baae0fc
Tom Gurion <nagasaki45@gmail.com>
2016-12-25 22:19:44 +0000

    CSS homepage improvements. Fix #26 Fix #72

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -9,4 +9,13 @@ defmodule Krihelinator.PageView do
     |> String.replace("_", " ")
     |> String.capitalize()
   end
+
+  @doc """
+  Takes a repo name and split and bold it like the repo names in github
+  trending.
+  """
+  def split_and_bold(string) do
+    [prefix, suffix] = String.split(string, "/")
+    {:safe, "#{prefix} / <b>#{suffix}</b>"}
+  end
 end
