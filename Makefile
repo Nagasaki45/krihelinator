6d80be2538bbd9a67b65d070f4f45406666fce27
Tom Gurion <nagasaki45@gmail.com>
2017-01-13 18:41:23 +0000

    Remove single line 'docker-compose run' from Makefile and improve testing

--- Makefile
+++ Makefile
@@ -6,14 +6,5 @@ deploy: build
 	docker push nagasaki45/krihelinator_web
 	# TODO continue with ssh.
 
-test:
-	docker-compose run --rm web mix test
-	docker-compose run --rm web mix credo
-
-iex:
-	docker-compose run --rm web iex -S mix
-
 logs:
 	docker logs -f krihelinator_web_1 | less +F
-
-.PHONY: test
--- mix.exs
+++ mix.exs
@@ -56,6 +56,6 @@ defmodule Krihelinator.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create --quiet", "ecto.migrate", "test", "credo"]]
   end
 end
