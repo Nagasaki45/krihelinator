7126c81018d19cc1190c285b0f06ae6000169cf8
Tom Gurion <nagasaki45@gmail.com>
2017-02-13 12:56:07 +0000

    Frontend: Better html titles. Fix #114

--- krihelinator/web/views/layout_view.ex
+++ krihelinator/web/views/layout_view.ex
@@ -8,4 +8,15 @@ defmodule Krihelinator.LayoutView do
   def navbar_link(assigns) do
     render "navbar_link.html", assigns
   end
+
+  def page_title(assigns) do
+    case action_name(assigns.conn) do
+      :languages -> "Krihelinator/languages"
+      :badge -> "Krihelinator/badges"
+      :about -> "Krihelinator/about"
+      :language -> "Krihelinator/#{assigns.language.name}"
+      :languages_history -> "Krihelinator/history"
+      _whatever -> "the Krihelinator"
+    end
+  end
 end
