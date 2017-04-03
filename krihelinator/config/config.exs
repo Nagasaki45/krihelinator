5286f9d7c8146e66888e10fa097f3b8fbe265d7d
Tom Gurion <nagasaki45@gmail.com>
2017-04-03 02:48:35 +0100

    Log dates. Fix #148

--- krihelinator/config/config.exs
+++ krihelinator/config/config.exs
@@ -22,7 +22,7 @@ config :krihelinator, Krihelinator.Endpoint,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format: "$date $time $metadata[$level] $message\n",
   metadata: [:request_id]
 
 # Configure your database
